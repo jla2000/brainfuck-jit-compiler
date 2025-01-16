c1cbf69d495e0c1e90fff8d84701c73e9927b8b7
Jan Lafferton <jan.lafferton@vector.com>
2025-01-16 15:58:26 +0000

    Working jit

--- src/main.zig
+++ src/main.zig
@@ -7,5 +7,21 @@ pub fn main() !void {
     var code = std.ArrayList(u8).init(allocator);
     defer code.deinit();
 
-    try code.append(0x00);
+    try code.appendSlice(&.{ 0x48, 0x31, 0xC0 }); // xor rax, rax
+    try code.append(0xC3); // ret
+
+    try execute_bytecode(code.items);
+}
+
+fn execute_bytecode(code: []u8) !void {
+    const page_buffer = try std.posix.mmap(null, code.len, std.posix.PROT.WRITE | std.posix.PROT.READ | std.posix.PROT.EXEC, .{
+        .ANONYMOUS = true,
+        .TYPE = .PRIVATE,
+    }, -1, 0);
+    defer std.posix.munmap(page_buffer);
+
+    @memcpy(page_buffer, code);
+
+    const ptr: *fn () void = @ptrCast(page_buffer.ptr);
+    ptr();
 }
