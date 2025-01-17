b175d200da38b89c23a41b950776a07ab510a7d4
Jan Lafferton <jan.lafferton@vector.com>
2025-01-17 12:29:25 +0000

    nice

--- src/main.zig
+++ src/main.zig
@@ -1,47 +1,64 @@
 const std = @import("std");
 
-const Instruction = enum(u8) {
-    INC_CELL,
-    DEC_CELL,
-    NEXT_CELL,
-    PREV_CELL,
-    READ_CELL,
-    WRITE_CELL,
-    JMP_IF_ZERO,
-    JMP_IF_NOT_ZERO,
-};
-
-const EntryPoint = fn (
-    tape_ptr: *const u8,
-) callconv(.C) void;
-
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     const allocator = gpa.allocator();
 
-    const code = try generate_bytecode(allocator, &.{ .INC_CELL, .NEXT_CELL, .INC_CELL, .PREV_CELL, .DEC_CELL });
+    const code = try generate_bytecode(allocator, ".,");
     defer code.deinit();
 
     try execute_bytecode(code.items);
 }
 
-fn generate_bytecode(allocator: std.mem.Allocator, instructions: []const Instruction) !std.ArrayList(u8) {
+fn write_handler(tape_ptr: *u8) callconv(.C) void {
+    std.debug.print("Write called with ptr={}\n", .{tape_ptr});
+}
+
+fn read_handler(tape_ptr: *u8) callconv(.C) void {
+    std.debug.print("Read called with ptr={}\n", .{tape_ptr});
+}
+
+fn generate_bytecode(allocator: std.mem.Allocator, instructions: []const u8) !std.ArrayList(u8) {
     var code = std.ArrayList(u8).init(allocator);
 
     const amount = 1;
 
     for (instructions) |instruction| {
         switch (instruction) {
-            .INC_CELL => try code.appendSlice(&.{ 0x80, 0x07, amount }), // add byte ptr [rdi], amount
-            .DEC_CELL => try code.appendSlice(&.{ 0x80, 0x2F, amount }), // sub byte ptr [rdi], amount
-            .NEXT_CELL => try code.appendSlice(&.{ 0x48, 0x83, 0xC7, amount }), // add rdi, amount
-            .PREV_CELL => try code.appendSlice(&.{ 0x48, 0x83, 0xEF, amount }), // sub rdi, amount
+            '+' => try code.appendSlice(&.{
+                0x80, 0x07, amount, // add byte ptr [rdi], amount
+            }),
+            '-' => try code.appendSlice(&.{
+                0x80, 0x2F, amount, // sub byte ptr [rdi], amount
+            }),
+            '>' => try code.appendSlice(&.{
+                0x48, 0x83, 0xC7, amount, // add rdi, amount
+            }),
+            '<' => try code.appendSlice(&.{
+                0x48, 0x83, 0xEF, amount, // sub rdi, amount
+            }),
+            '.' => try code.appendSlice(&.{
+                0x57, // push rdi
+                0x56, // push rsi
+                0x52, // push rdx
+                0xFF, 0xD6, // call rsi
+                0x5A, // pop rdx
+                0x5E, // pop rsi
+                0x5F, // pop rdi
+            }),
+            ',' => try code.appendSlice(&.{
+                0x57, // push rdi
+                0x56, // push rsi
+                0x52, // push rdx
+                0xFF, 0xD2, // call rdx
+                0x5A, // pop rdx
+                0x5E, // pop rsi
+                0x5F, // pop rdi
+            }),
             else => {},
         }
     }
 
-    try code.appendSlice(&.{ 0x48, 0x83, 0xC7, 0x02 }); // add rdi, 0x2
-    try code.appendSlice(&.{ 0xC6, 0x07, 0xFF }); // mov BYTE PTR [rdi], 0xff
     try code.append(0xC3); // ret
 
     return code;
@@ -57,8 +74,12 @@ fn execute_bytecode(code: []u8) !void {
     @memcpy(page_buffer, code);
 
     var tape = std.mem.zeroes([30000]u8);
-    const execute_code: *const EntryPoint = @ptrCast(page_buffer.ptr);
-    execute_code(@ptrCast(&tape));
 
-    std.debug.print("0: {}, 1: {}\n", .{ tape[0], tape[1] });
+    const execute_code: *const fn (
+        tape_ptr: *const u8,
+        write_fn: *const fn (*u8) callconv(.C) void,
+        read_fn: *const fn (*u8) callconv(.C) void,
+    ) callconv(.C) void = @ptrCast(page_buffer.ptr);
+
+    execute_code(@ptrCast(&tape), &write_handler, &read_handler);
 }
